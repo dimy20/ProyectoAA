d8afc3c99c48505db5d9ffb6eab71c97db895c9f
dimy20 <enzowstm@gmail.com>
2026-08-30 23:02:38 -0300

    damn commas

--- tools/data.py
+++ tools/data.py
@@ -19,8 +19,8 @@ BUCKET_NAME = "proyecto-water-aa"
 SRC_DATA_DIR = "data" 
 
 def get_s3_client():
-  aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-  aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+  aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"]
+  aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
   region_name=os.environ.get("AWS_REGION", "us-east-1")
 
   if not aws_access_key_id or not aws_secret_access_key or not region_name:
